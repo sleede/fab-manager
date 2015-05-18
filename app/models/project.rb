4686a8c84305e25047dc68998900b6818ae5ebc5
Nicolas Florentin <nicolas@sleede.com>
2015-05-18 10:16:21 +0200

    rejects project cao file if all blank

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,7 +8,7 @@ class Project < ActiveRecord::Base
   has_one :project_image, as: :viewable, dependent: :destroy
   accepts_nested_attributes_for :project_image, allow_destroy: true
   has_many :project_caos, as: :viewable, dependent: :destroy
-  accepts_nested_attributes_for :project_caos, allow_destroy: true
+  accepts_nested_attributes_for :project_caos, allow_destroy: true, reject_if: :all_blank
 
   has_and_belongs_to_many :machines, join_table: :projects_machines
   has_and_belongs_to_many :components, join_table: :projects_components
