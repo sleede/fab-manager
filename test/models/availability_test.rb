26e3f5bf8c0223127a119d24aa6970017b863375
Nicolas Florentin <nicolas@sleede.com>
2016-04-05 10:03:58 +0200

    adds a test to availability model test

--- test/models/availability_test.rb
+++ test/models/availability_test.rb
@@ -6,4 +6,10 @@ class AvailabilityTest < ActiveSupport::TestCase
     assert a.invalid?
     assert a.errors.key?(:end_at)
   end
+
+  test "if type available_type is 'machines' check that there is minimum 1 association" do
+    a = Availability.new(start_at: Time.now, end_at: 2.hours.from_now, available_type: 'machines')
+    assert a.invalid?
+    assert a.errors.key?(:machine_ids)
+  end
 end
