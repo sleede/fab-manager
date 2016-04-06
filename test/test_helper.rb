f334f76ef8d4417399a8ab144b2f488482dcd13c
Nicolas Florentin <nicolas@sleede.com>
2016-04-06 16:26:45 +0200

    improves stripe_card_token test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -29,14 +29,31 @@ class ActiveSupport::TestCase
     { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
   end
 
-  def stripe_card_token
-    Stripe::Token.create(
-        :card => {
-            :number => "4242424242424242",
-            :exp_month => 4,
-            :exp_year => DateTime.now.next_year.year,
-            :cvc => "314"
-        },
+  def stripe_card_token(error: nil)
+    number = "4242424242424242"
+    exp_month = 4
+    exp_year = DateTime.now.next_year.year
+    cvc = "314"
+
+    case error
+    when /card_declined/
+      number = "4000000000000002"
+    when /incorrect_number/
+      number = "4242424242424241"
+    when /invalid_expiry_month/
+      exp_month = 15
+    when /invalid_expiry_year/
+      exp_year = 1964
+    when /invalid_cvc/
+      cvc = "99"
+    end
+
+    Stripe::Token.create(card: {
+      number: number,
+        exp_month: exp_month,
+        exp_year: exp_year,
+        cvc:  cvc
+      },
     ).id
   end
 end
