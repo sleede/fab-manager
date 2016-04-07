2ab8d04a689d51fb36e7b211001933465bdd9d2d
Nicolas Florentin <nicolas@sleede.com>
2016-04-07 16:00:12 +0200

    adds comment to model invoice_item

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,5 +2,5 @@ class InvoiceItem < ActiveRecord::Base
   belongs_to :invoice
   belongs_to :subscription
 
-  has_one :invoice_item
+  has_one :invoice_item # to associated invoice_items of an invoice to invoice_items of an avoir
 end
