3016ac1e82662344b36062c6b73080387d6d1e5b
Sylvain <bond.never.die@gmail.com>
2016-04-13 17:26:17 +0200

    tests for availabilities

---
+++ test/integration/availabilities/as_admin_test.rb
@@ -0,0 +1,45 @@
+module Availabilities
+  class AsAdminTest < ActionDispatch::IntegrationTest
+    setup do
+      admin = User.with_role(:admin).first
+      login_as(admin, scope: :user)
+    end
+
+    test 'return availability by id' do
+      a = Availability.take
+
+      get "/api/availabilities/#{a.id}"
+
+      # Check response format & status
+      assert_equal 200, response.status
+      assert_equal Mime::JSON, response.content_type
+
+      # Check the correct availability was returned
+      availability = json_response(response.body)
+      assert_equal a.id, availability[:id], 'availability id does not match'
+    end
+
+    test 'get machine availabilities as admin' do
+      m = Machine.find_by_slug('decoupeuse-vinyle')
+
+      get "/api/availabilities/machines/#{m.id}"
+
+      # Check response format & status
+      assert_equal 200, response.status
+      assert_equal Mime::JSON, response.content_type
+
+      # Check the correct availabilities was returned
+      availabilities = json_response(response.body)
+      assert_not_empty availabilities, 'no availabilities were found'
+      assert_not_nil availabilities[0], 'first availability was unexpectedly nil'
+      assert_not_nil availabilities[0][:machine], "first availability's machine was unexpectedly nil"
+      assert_equal m.id, availabilities[0][:machine][:id], "first availability's machine does not match the required machine"
+
+      # Check that we din't get availabilities from the past
+      availabilities.each do |a|
+        assert_not a[:start] < DateTime.now, 'retrieved a slot in the past'
+      end
+    end
+end
+end
+
--- test/integration/availabilities/as_user_test.rb
+++ test/integration/availabilities/as_user_test.rb
@@ -1,24 +1,10 @@
-class AvailabilitiesTest < ActionDispatch::IntegrationTest
+class Availabilities::AsUserTest < ActionDispatch::IntegrationTest
   setup do
-    admin = User.with_role(:admin).first
-    login_as(admin, scope: :user)
+    user = User.find_by_username('kdumas')
+    login_as(user, scope: :user)
   end
 
-  test 'return availability by id' do
-    a = Availability.take
-
-    get "/api/availabilities/#{a.id}"
-
-    # Check response format & status
-    assert_equal 200, response.status
-    assert_equal Mime::JSON, response.content_type
-
-    # Check the correct availability was returned
-    availability = json_response(response.body)
-    assert_equal a.id, availability[:id], 'availability id does not match'
-  end
-
-  test 'get machine availabilities' do
+  test 'get machine availabilities as user' do
     m = Machine.find_by_slug('decoupeuse-vinyle')
 
     get "/api/availabilities/machines/#{m.id}"
@@ -33,5 +19,16 @@ class AvailabilitiesTest < ActionDispatch::IntegrationTest
     assert_not_nil availabilities[0], 'first availability was unexpectedly nil'
     assert_not_nil availabilities[0][:machine], "first availability's machine was unexpectedly nil"
     assert_equal m.id, availabilities[0][:machine][:id], "first availability's machine does not match the required machine"
+
+    # Check that we din't get availabilities from the past
+    availabilities.each do |a|
+      assert_not a[:start] < DateTime.now, 'retrieved a slot in the past'
+    end
+
+    # Check that we don't get availabilities in more than a month
+    availabilities.each do |a|
+      assert_not a[:start] > 1.month.from_now , 'retrieved a slot in more than 1 month for user who has no yearly subscription'
+    end
   end
 end
+
