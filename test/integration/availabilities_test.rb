c0f57cc213b6b54d32a15a31ef9bd35cb85dae5e
Sylvain <bond.never.die@gmail.com>
2016-04-13 11:53:21 +0200

    [ongoing] availability tests

--- test/integration/availabilities_test.rb
+++ test/integration/availabilities_test.rb
@@ -4,10 +4,23 @@ class AvailabilitiesTest < ActionDispatch::IntegrationTest
     login_as(admin, scope: :user)
   end
 
-  test "return availability by id" do
+  test 'return availability by id' do
     a = Availability.take
 
     get "/api/availabilities/#{a.id}"
+
+    # Check response format & status
     assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+
+    # Check the correct availability was returned
+    availability = json_response(response.body)
+    assert_equal a.id, availability[:id], 'availability id does not match'
+  end
+
+  test 'get machine availabilities' do
+    m = Machine.find_by_slug('decoupeuse-vinyle')
+
+    get "/api/availabilities/machines/#{m.id}"
   end
 end
