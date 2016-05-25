60c2813991744d9c6eabf9c2719576c046898228
Nicolas Florentin <nicolas@sleede.com>
2016-05-25 12:21:38 +0200

    prepares schedule.yml to code insertion

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -14,7 +14,8 @@ Sidekiq.configure_server do |config|
   schedule_file = "config/schedule.yml"
 
   if File.exists?(schedule_file)
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+    rendered_schedule_file = ERB.new(File.read(schedule_file)).result
+    Sidekiq::Cron::Job.load_from_hash YAML.load(rendered_schedule_file)
   end
 end
 
