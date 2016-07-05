b22bae1d8f97c4da318a51c3841a0eb446cd4f48
Peng DU <peng@sleede.com>
2016-07-05 11:32:06 +0200

    wallet amount convert auto to float

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -4,6 +4,14 @@ class Wallet < ActiveRecord::Base
   validates :user, presence: true
   validates_numericality_of :amount, greater_than_or_equal_to: 0
 
+  def amount=(amount)
+    write_attribute(:amount, amount.to_i * 100)
+  end
+
+  def amount
+    read_attribute(:amount) / 100.0
+  end
+
   def credit(amount)
     if amount.is_a?(Numeric) and amount >= 0
       self.amount += amount
