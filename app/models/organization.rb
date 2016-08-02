c9a7c599c097e6d3dddd98e146f86dbd93bf16a1
Sylvain <bond.never.die@gmail.com>
2016-08-02 18:06:07 +0200

    notify admin about organization registration & organization server validation

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,7 @@ class Organization < ActiveRecord::Base
   belongs_to :profile
   has_one :address, as: :placeable, dependent: :destroy
   accepts_nested_attributes_for :address, allow_destroy: true
+
+  validates :name, presence: true
+  validates :address, presence: true
 end
