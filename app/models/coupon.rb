8aa5d5e4f54f9c258e8972f8d430f9bfcefa3beb
Sylvain <bond.never.die@gmail.com>
2016-08-17 09:53:58 +0200

    allow dashes in coupon codes

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -6,7 +6,7 @@ class Coupon < ActiveRecord::Base
 
   validates :name, presence: true
   validates :code, presence: true
-  validates :code, format: { with: /\A[A-Z0-9]+\z/ ,message: 'only caps letters and numbers'}
+  validates :code, format: { with: /\A[A-Z0-9\-]+\z/ ,message: 'only caps letters and numbers'}
   validates :code, uniqueness: true
   validates :percent_off, presence: true
   validates :percent_off, :inclusion => 0..100
