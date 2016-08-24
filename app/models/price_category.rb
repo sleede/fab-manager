77db2b2326d6856197ecd4e7600d1d83b08258f6
Sylvain <bond.never.die@gmail.com>
2016-08-24 16:52:37 +0200

    ability to edit price category

--- app/models/price_category.rb
+++ app/models/price_category.rb
@@ -2,6 +2,10 @@ class PriceCategory < ActiveRecord::Base
   has_many :event_price_category
   has_many :events, through: :event_price_categories
 
+  validates :name, :presence => true
+  validates :name, uniqueness: {case_sensitive: false}
+  validates :conditions, :presence => true
+
   def safe_destroy
     if event_price_category.count == 0
       destroy
