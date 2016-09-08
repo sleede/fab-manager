0307972f9c6d445638ffdc2ec6ab481172c48ac8
Sylvain <bond.never.die@gmail.com>
2016-09-08 16:58:18 +0200

    add tests about event price's categories

--- test/models/price_category_test.rb
+++ test/models/price_category_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PriceCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'price category name must be unique' do
+    pc = PriceCategory.new({name: '- DE 25 ANS', conditions: 'Tarif préférentiel pour les jeunes'})
+    assert pc.invalid?
+    assert pc.errors[:name].present?
+  end
+
+  test 'associated price category cannot be destroyed' do
+    pc = PriceCategory.find(1)
+    assert_not pc.safe_destroy
+    assert_not_empty PriceCategory.where(id: 1)
+  end
 end
