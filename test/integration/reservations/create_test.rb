c6d9cbd88d668aae744e1a1fc3ea279607d6df31
Sylvain <bond.never.die@gmail.com>
2016-09-08 14:26:21 +0200

    add integration test for coupon

--- test/integration/reservations/create_test.rb
+++ test/integration/reservations/create_test.rb
@@ -5,7 +5,7 @@ module Reservations
       @user_with_subscription = User.with_role(:member).with_subscription.second
     end
 
-    test "user without subscription reserves a machine with success" do
+    test 'user without subscription reserves a machine with success' do
       login_as(@user_without_subscription, scope: :user)
 
       machine = Machine.find(6)
@@ -15,8 +15,9 @@ module Reservations
       invoice_count = Invoice.count
       invoice_items_count = InvoiceItem.count
       users_credit_count = UsersCredit.count
+      subscriptions_count = Subscription.count
 
-      VCR.use_cassette("reservations_create_for_machine_without_subscription_success") do
+      VCR.use_cassette('reservations_create_for_machine_without_subscription_success') do
         post reservations_path, { reservation: {
             user_id: @user_without_subscription.id,
             reservable_id: machine.id,
@@ -37,6 +38,7 @@ module Reservations
       assert_equal invoice_count + 1, Invoice.count
       assert_equal invoice_items_count + 1, InvoiceItem.count
       assert_equal users_credit_count, UsersCredit.count
+      assert_equal subscriptions_count, Subscription.count
 
       # reservation assertions
       reservation = Reservation.last
@@ -65,7 +67,7 @@ module Reservations
       assert_not_empty Notification.where(attached_object: reservation)
     end
 
-    test "user without subscription reserves a machine with error" do
+    test 'user without subscription reserves a machine with error' do
       login_as(@user_without_subscription, scope: :user)
 
       machine = Machine.find(6)
@@ -76,7 +78,7 @@ module Reservations
       invoice_items_count = InvoiceItem.count
       notifications_count = Notification.count
 
-      VCR.use_cassette("reservations_create_for_machine_without_subscription_error") do
+      VCR.use_cassette('reservations_create_for_machine_without_subscription_error') do
         post reservations_path, { reservation: {
             user_id: @user_without_subscription.id,
             reservable_id: machine.id,
@@ -99,7 +101,7 @@ module Reservations
       assert_equal notifications_count, Notification.count
     end
 
-    test "user without subscription reserves a training with success" do
+    test 'user without subscription reserves a training with success' do
       login_as(@user_without_subscription, scope: :user)
 
       training = Training.first
@@ -109,7 +111,7 @@ module Reservations
       invoice_count = Invoice.count
       invoice_items_count = InvoiceItem.count
 
-      VCR.use_cassette("reservations_create_for_training_without_subscription_success") do
+      VCR.use_cassette('reservations_create_for_training_without_subscription_success') do
         post reservations_path, { reservation: {
             user_id: @user_without_subscription.id,
             reservable_id: training.id,
@@ -157,7 +159,7 @@ module Reservations
       assert_not_empty Notification.where(attached_object: reservation)
     end
 
-    test "user with subscription reserves a machine with success" do
+    test 'user with subscription reserves a machine with success' do
       login_as(@user_with_subscription, scope: :user)
 
       plan = @user_with_subscription.subscribed_plan
@@ -169,7 +171,7 @@ module Reservations
       invoice_items_count = InvoiceItem.count
       users_credit_count = UsersCredit.count
 
-      VCR.use_cassette("reservations_create_for_machine_with_subscription_success") do
+      VCR.use_cassette('reservations_create_for_machine_with_subscription_success') do
         post reservations_path, { reservation: {
             user_id: @user_with_subscription.id,
             reservable_id: machine.id,
@@ -230,7 +232,7 @@ module Reservations
       assert_not_empty Notification.where(attached_object: reservation)
     end
 
-    test "user with subscription reserves the FIRST training with success" do
+    test 'user with subscription reserves the FIRST training with success' do
       login_as(@user_with_subscription, scope: :user)
       plan = @user_with_subscription.subscribed_plan
       plan.update!(is_rolling: true)
@@ -242,7 +244,7 @@ module Reservations
       invoice_count = Invoice.count
       invoice_items_count = InvoiceItem.count
 
-      VCR.use_cassette("reservations_create_for_training_with_subscription_success") do
+      VCR.use_cassette('reservations_create_for_training_with_subscription_success') do
         post reservations_path, { reservation: {
             user_id: @user_with_subscription.id,
             reservable_id: training.id,
@@ -293,7 +295,7 @@ module Reservations
       assert_equal reservation.slots.first.start_at + plan.duration, @user_with_subscription.subscription.expired_at
     end
 
-    test "user reserves a machine and pay by wallet with success" do
+    test 'user reserves a machine and pay by wallet with success' do
       @vlonchamp = User.find_by(username: 'vlonchamp')
       login_as(@vlonchamp, scope: :user)
 
@@ -306,7 +308,7 @@ module Reservations
       users_credit_count = UsersCredit.count
       wallet_transactions_count = WalletTransaction.count
 
-      VCR.use_cassette("reservations_create_for_machine_and_pay_wallet_success") do
+      VCR.use_cassette('reservations_create_for_machine_and_pay_wallet_success') do
         post reservations_path, { reservation: {
             user_id: @vlonchamp.id,
             reservable_id: machine.id,
@@ -364,7 +366,7 @@ module Reservations
       assert_equal transaction.amount, invoice.wallet_amount / 100.0
     end
 
-    test "user reserves a training and plan by wallet with success" do
+    test 'user reserves a training and plan by wallet with success' do
       @vlonchamp = User.find_by(username: 'vlonchamp')
       login_as(@vlonchamp, scope: :user)
 
@@ -377,7 +379,7 @@ module Reservations
       invoice_items_count = InvoiceItem.count
       wallet_transactions_count = WalletTransaction.count
 
-      VCR.use_cassette("reservations_create_for_training_and_plan_by_pay_wallet_success") do
+      VCR.use_cassette('reservations_create_for_training_and_plan_by_pay_wallet_success') do
         post reservations_path, { reservation: {
             user_id: @user_without_subscription.id,
             reservable_id: training.id,
@@ -429,6 +431,91 @@ module Reservations
       assert_equal transaction.transaction_type, 'debit'
       assert_equal transaction.amount, 10
       assert_equal transaction.amount, invoice.wallet_amount / 100.0
+    end
+
+    test 'user reserves a machine and a subscription using a coupon with success' do
+      login_as(@user_without_subscription, scope: :user)
+
+      machine = Machine.find(6)
+      plan = Plan.where(group_id: @user_without_subscription.group_id).first
+      availability = machine.availabilities.first
+
+      reservations_count = Reservation.count
+      invoice_count = Invoice.count
+      invoice_items_count = InvoiceItem.count
+      subscriptions_count = Subscription.count
+      users_credit_count = UsersCredit.count
+
+      VCR.use_cassette('reservations_machine_and_plan_using_coupon_success') do
+        post reservations_path, {
+          reservation: {
+            user_id: @user_without_subscription.id,
+            reservable_id: machine.id,
+            reservable_type: machine.class.name,
+            card_token: stripe_card_token,
+            slots_attributes: [
+                { start_at: availability.start_at.to_s(:iso8601),
+                  end_at: (availability.start_at + 1.hour).to_s(:iso8601),
+                  availability_id: availability.id
+                }
+            ],
+            plan_id: plan.id
+          },
+          coupon_code: 'SUNNYFABLAB'
+        }.to_json, default_headers
+      end
+
+      # general assertions
+      assert_equal 201, response.status
+      assert_equal reservations_count + 1, Reservation.count
+      assert_equal invoice_count + 1, Invoice.count
+      assert_equal invoice_items_count + 2, InvoiceItem.count
+      assert_equal users_credit_count, UsersCredit.count
+      assert_equal subscriptions_count + 1, Subscription.count
+
+      # reservation assertions
+      reservation = Reservation.last
+
+      assert reservation.invoice
+      refute reservation.stp_invoice_id.blank?
+      assert_equal 2, reservation.invoice.invoice_items.count
+
+      # invoice assertions
+      invoice = reservation.invoice
+
+      refute invoice.stp_invoice_id.blank?
+      refute invoice.total.blank?
+
+      # invoice_items assertions
+      ## reservation
+      reservation_item = invoice.invoice_items.where(subscription_id: nil).first
+
+      assert_not_nil reservation_item
+      assert reservation_item.stp_invoice_item_id
+      assert_equal reservation_item.amount, machine.prices.find_by(group_id: @user_without_subscription.group_id, plan_id: plan.id).amount
+      ## subscription
+      subscription_item = invoice.invoice_items.where.not(subscription_id: nil).first
+
+      assert_not_nil subscription_item
+
+      subscription = Subscription.find(subscription_item.subscription_id)
+
+      assert subscription_item.stp_invoice_item_id
+      assert_equal subscription_item.amount, plan.amount
+      assert_equal subscription.plan_id, plan.id
+
+      # invoice assertions
+      invoice = Invoice.find_by(invoiced: reservation)
+      assert_invoice_pdf invoice
+
+      VCR.use_cassette('reservations_machine_and_plan_using_coupon_retrieve_invoice_from_stripe') do
+        stp_invoice = Stripe::Invoice.retrieve(invoice.stp_invoice_id)
+        assert_equal stp_invoice.total, invoice.total
+      end
+
+      # notifications
+      assert_not_empty Notification.where(attached_object: reservation)
+      assert_not_empty Notification.where(attached_object: subscription)
     end
   end
 end
