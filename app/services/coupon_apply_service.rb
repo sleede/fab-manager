3dc5f1a0e47ed7d56997018e7efe9f46eb2d1d80
Sylvain <bond.never.die@gmail.com>
2016-11-24 11:33:45 +0100

    real fix for coupon change slot's original price (reverts cfd92cdc40)

--- app/services/coupon_apply_service.rb
+++ app/services/coupon_apply_service.rb
@@ -1,11 +1,11 @@
 class CouponApplyService
-  def call(total, coupon_code)
+  def call(total, coupon_code, user_id = nil)
     price = total
 
     # if no coupon code or if code does not match, return origin price without change
     unless coupon_code.nil?
       _coupon = Coupon.find_by(code: coupon_code)
-      if _coupon
+      if not _coupon.nil? and _coupon.status(user_id) == 'active'
         if _coupon.type == 'percent_off'
           price = price - (price * _coupon.percent_off / 100.0)
         elsif _coupon.type == 'amount_off'
