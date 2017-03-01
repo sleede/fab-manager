cf9181770bdd434a8d9a87de57d0f0b39ce9d459
Sylvain <bond.never.die@gmail.com>
2017-03-01 12:56:11 +0100

    export space statistics

--- app/policies/statistic_policy.rb
+++ app/policies/statistic_policy.rb
@@ -1,6 +1,6 @@
 class StatisticPolicy < ApplicationPolicy
   %w(index account event machine project subscription training user space scroll export_subscription export_machine
-     export_training export_event export_account export_project export_global).each do |action|
+     export_training export_event export_account export_project export_space export_global).each do |action|
     define_method "#{action}?" do
       user.is_admin?
     end
