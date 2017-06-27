cf2da2703c54aaf2f1c5b529acbc6a71a1827807
Michael JIN <emj365@gmail.com>
2017-06-27 20:15:45 +0800

    add codes to allow cors of machines list

--- app/controllers/api/machines_controller.rb
+++ app/controllers/api/machines_controller.rb
@@ -1,7 +1,29 @@
 class API::MachinesController < API::ApiController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_machine, only: [:update, :destroy]
+
+  before_action :cors_preflight_check, only: [:index]
+  after_action :cors_set_access_control_headers, only: [:index]
+
   respond_to :json
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
 
   def index
     @machines = Machine.includes(:machine_image, :plans)
