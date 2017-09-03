73c9e301fbb3642e074ab1661c29bd9a2aa57268
siteswapjuggler <garnav@wanadoo.fr>
2017-09-03 13:55:38 +0200

    open api events improvement
    new data available: start_at, end_at, event_image
    new controllers: id, upcoming, limit

--- app/controllers/open_api/v1/events_controller.rb
+++ app/controllers/open_api/v1/events_controller.rb
@@ -3,16 +3,34 @@ class OpenAPI::V1::EventsController < OpenAPI::V1::BaseController
   expose_doc
   
   def index
-    @events = Event.order(created_at: :desc)
+    
+	if upcoming
+      @events = Event.includes(:event_image, :event_files, :availability, :category)
+				.where('availabilities.end_at >= ?', Time.now)
+                .order('availabilities.start_at ASC').references(:availabilities)
+    else
+	  @events = Event.includes(:event_image, :event_files, :availability, :category).order(created_at: :desc)
+    end
+
+    if params[:id].present?
+      @events = @events.where(id: params[:id])
+	end	
 
     if params[:page].present?
       @events = @events.page(params[:page]).per(per_page)
       paginate @events, per_page: per_page
     end
+	
+	if params[:limit].present?
+	  @events = @events.limit(params[:limit])
+	end
   end
 
   private
     def per_page
       params[:per_page] || 20
     end
+	def upcoming
+	  params[:upcoming] || false
+	end
 end
