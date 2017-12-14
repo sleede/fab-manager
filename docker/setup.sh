fea78f2995c6c9bd761eb0924484092e75c4705b
Sylvain <bond.never.die@gmail.com>
2017-12-14 11:58:23 +0100

    setup script for docker installation

---
+++ docker/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+prepare_config()
+{
+  FABMANAGER_PATH = ${1:-/apps/fabmanager/config}
+
+  mkdir -p "$FABMANAGER_PATH/example"
+
+  # fab-manager environment variables
+  \curl -sSL https://raw.githubusercontent.com/LaCasemate/fab-manager/master/docker/env.example > "$FABMANAGER_PATH/example/env.example"
+
+  # nginx configuration
+  \curl -sSL https://raw.githubusercontent.com/LaCasemate/fab-manager/master/docker/nginx_with_ssl.conf.example > "$FABMANAGER_PATH/example/nginx_with_ssl.conf.example"
+  \curl -sSL https://raw.githubusercontent.com/LaCasemate/fab-manager/master/docker/nginx.conf.example > "$FABMANAGER_PATH/example/nginx.conf.example"
+
+  # let's encrypt configuration
+  \curl -sSL https://raw.githubusercontent.com/LaCasemate/fab-manager/master/docker/webroot.ini.example > "$FABMANAGER_PATH/example/webroot.ini.example"
+
+  # docker-compose
+  \curl -sSL https://github.com/LaCasemate/fab-manager/blob/master/docker/docker-compose.yml > "$FABMANAGER_PATH/docker-compose.yml"
+}
+
+prepare_config "$@"
