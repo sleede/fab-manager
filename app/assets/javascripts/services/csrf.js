ad3105cbc9a9202f0e851c9a0d7f6b6c838ab0b5
Sylvain <bond.never.die@gmail.com>
2018-11-27 13:57:41 +0100

    migrate to angular 1.4

--- app/assets/javascripts/services/csrf.js
+++ app/assets/javascripts/services/csrf.js
@@ -12,9 +12,9 @@ Application.Services.service('CSRF', ['$cookies',
       setMetaTags () {
         if (angular.element('meta[name="csrf-param"]').length === 0) {
           angular.element('head').append('<meta name="csrf-param" content="authenticity_token">');
-          angular.element('head').append(`<meta name=\"csrf-token\" content=\"${$cookies['XSRF-TOKEN']}\">`);
+          angular.element('head').append(`<meta name=\"csrf-token\" content=\"${$cookies.get('XSRF-TOKEN')}\">`);
         } else {
-          angular.element('meta[name="csrf-token"]').replaceWith(`<meta name=\"csrf-token\" content=\"${$cookies['XSRF-TOKEN']}\">`);
+          angular.element('meta[name="csrf-token"]').replaceWith(`<meta name=\"csrf-token\" content=\"${$cookies.get('XSRF-TOKEN')}\">`);
         }
       }
     })
