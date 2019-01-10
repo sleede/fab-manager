d00dfad8d8b62408531d95ed7237a4754265a480
Sylvain <bond.never.die@gmail.com>
2019-01-10 12:40:42 +0100

    improved invoices test + added fixtures

--- test/integration/invoices/as_admin_test.rb
+++ test/integration/invoices/as_admin_test.rb
@@ -10,7 +10,6 @@ class InvoicesTest < ActionDispatch::IntegrationTest
   end
 
   test 'admin list invoices' do
-
     post '/api/invoices/list', { query: {
       number: '',
       customer: '',
@@ -31,6 +30,36 @@ class InvoicesTest < ActionDispatch::IntegrationTest
     # Check that invoices are ordered by reference
     assert_equal '1604002', invoices.first[:reference]
     assert_equal '1203001', invoices.last[:reference]
+  end
+
+  test 'admin generates a refund' do
+    date = DateTime.now.iso8601
+
+    post '/api/invoices', { avoir: {
+      avoir_date: date,
+      avoir_mode: 'cash',
+      description: 'Lorem ipsum',
+      invoice_id: 4,
+      invoice_items_ids: [4],
+      subscription_to_expire: false
+    } }.to_json, default_headers
+
+    # Check response format & status
+    assert_equal 201, response.status, response.body
+    assert_equal Mime::JSON, response.content_type
+
+    # Check that the refund match
+    refund = json_response(response.body)
+    avoir = Avoir.find(refund[:id])
+
+    assert_dates_equal date, refund[:avoir_date]
+    assert_dates_equal date, refund[:date]
+    assert_equal 'cash', refund[:avoir_mode]
+    assert_equal false, refund[:has_avoir]
+    assert_equal 4, refund[:invoice_id]
+    assert_equal 4, refund[:items][0][:invoice_item_id]
+    assert_match %r{^[0-9]+/A$}, refund[:reference]
+    assert_equal 'Lorem ipsum', avoir.description
   end
 
 end
