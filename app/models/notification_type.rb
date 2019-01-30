a528fc37667b327928a349168000594ec6de1a97
Sylvain <bond.never.die@gmail.com>
2019-01-30 10:03:53 +0100

    infos about deprecated notification

--- app/models/notification_type.rb
+++ app/models/notification_type.rb
@@ -43,5 +43,7 @@ class NotificationType
     notify_member_about_coupon
     notify_member_reservation_reminder
   ]
-  # deprecated: notify_admin_invoicing_changed
+  # deprecated:
+  # - notify_member_subscribed_plan_is_changed
+  # - notify_admin_invoicing_changed
 end
