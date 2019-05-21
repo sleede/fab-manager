7bbea22e76490e6947f82fea2de81999c3da7e75
Sylvain <bond.never.die@gmail.com>
2019-05-21 11:49:50 +0200

    display reports in interface

--- app/assets/javascripts/controllers/admin/abuses.js
+++ app/assets/javascripts/controllers/admin/abuses.js
@@ -3,7 +3,21 @@
  */
 Application.Controllers.controller('AbusesController', ['$scope', '$state', 'Abuse', 'abusesPromise', 'growl', '_t',
   function ($scope, $state, Abuse, abusesPromise, growl, _t) {
+    /* PUBLIC SCOPE */
+
     // List of all reported abuses
-    $scope.abuses = abusesPromise.abuses;
+    $scope.abuses = [];
+
+    /* PRIVATE SCOPE */
+    /**
+     * Kind of constructor: these actions will be realized first when the controller is loaded
+     */
+    const initialize = function () {
+      // we display only abuses related to projects
+      $scope.abuses = abusesPromise.abuses.filter(a => a.signaled_type === 'Project');
+    };
+
+    // !!! MUST BE CALLED AT THE END of the controller
+    return initialize();
   }
 ]);
--- app/views/api/abuses/index.json.jbuilder
+++ app/views/api/abuses/index.json.jbuilder
@@ -1,5 +1,17 @@
 json.abuses do
   json.array!(@abuses) do |abuse|
-    json.extract! abuse, :id, :signaled_id, :signaled_type
+    json.extract! abuse, :id, :signaled_id, :signaled_type, :first_name, :last_name, :email, :message, :created_at
+    case abuse.signaled_type
+    when 'Project'
+      json.signaled do
+        json.extract! abuse.signaled, :name, :slug, :published_at
+        json.author do
+          json.id abuse.signaled.author.id
+          json.full_name abuse.signaled.author.profile.full_name
+        end
+      end
+    else
+      json.signaled abuse.signaled
+    end
   end
 end
