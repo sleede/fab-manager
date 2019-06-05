9fd6ea08b9c0bdc32788457bd6e6829f8211a8f9
Sylvain <bond.never.die@gmail.com>
2019-06-05 07:52:02 +0200

    fix SubscriptionGroupValidator

--- app/validators/subscription_group_validator.rb
+++ app/validators/subscription_group_validator.rb
@@ -1,6 +1,6 @@
 class SubscriptionGroupValidator < ActiveModel::Validator
   def validate(record)
-    return if record.statistic_profile.group_id == record.plan.group
+    return if record.statistic_profile.group_id == record.plan.group_id
 
     record.errors[:plan_id] << "This plan is not compatible with the current user's group"
   end
