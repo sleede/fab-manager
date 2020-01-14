a564ec62b5edbcc2ae9d4ed80584eeeb1feaa6c2
Sylvain <bond.never.die@gmail.com>
2020-01-14 16:16:25 +0100

    updated version check worker

--- app/workers/version_check_worker.rb
+++ app/workers/version_check_worker.rb
@@ -9,7 +9,15 @@ class VersionCheckWorker
     res = FabHub.fab_manager_version_check
 
     setting_ver = Setting.find_or_initialize_by(name: 'hub_last_version')
-    setting_ver.value = { version: res['last_version']['semver'], security: res['last_version']['security'], status: res['up_to_date'] }.to_json.to_s
+    value = {
+      security: res['status']['security'],
+      up_to_date: res['status']['up_to_date']
+    }
+    if res['upgrade_to']
+      value['version'] = res['upgrade_to']['semver']
+      value['url'] = res['upgrade_to']['url']
+    end
+    setting_ver.value = value.to_json.to_s
     setting_ver.save!
 
     setting_key = Setting.find_or_initialize_by(name: 'hub_public_key')
