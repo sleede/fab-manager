372d7f936b26af8a3b855f7a636ca1fe240f5c22
Sylvain <bond.never.die@gmail.com>
2020-02-11 11:46:40 +0100

    typos & syntax

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 20200206132857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "unaccent"
   enable_extension "pg_trgm"
+  enable_extension "unaccent"
 
   create_table "abuses", force: :cascade do |t|
     t.integer  "signaled_id"
