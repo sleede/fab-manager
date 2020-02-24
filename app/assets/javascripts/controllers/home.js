76c1b69b7a9343956850479fec76715a98aa895d
Sylvain <sylvain@sleede.com>
2020-02-24 16:51:03 +0100

    machines tour

--- app/assets/javascripts/controllers/home.js
+++ app/assets/javascripts/controllers/home.js
@@ -82,8 +82,8 @@ Application.Controllers.controller('HomeController', ['$scope', '$stateParams', 
         selector: 'body',
         stepId: 'welcome',
         order: 0,
-        title: _t('app.public.tour.welcome.title'),
-        content: _t('app.public.tour.welcome.content'),
+        title: _t('app.public.tour.welcome.welcome.title'),
+        content: _t('app.public.tour.welcome.welcome.content'),
         placement: 'bottom',
         orphan: true
       });
@@ -91,24 +91,24 @@ Application.Controllers.controller('HomeController', ['$scope', '$stateParams', 
         selector: '.nav-primary li.home-link',
         stepId: 'home',
         order: 1,
-        title: _t('app.public.tour.home.title'),
-        content: _t('app.public.tour.home.content'),
+        title: _t('app.public.tour.welcome.home.title'),
+        content: _t('app.public.tour.welcome.home.content'),
         placement: 'right'
       });
       uitour.createStep({
         selector: '.nav-primary li.reserve-machine-link',
         stepId: 'machines',
         order: 2,
-        title: _t('app.public.tour.machines.title'),
-        content: _t('app.public.tour.machines.content'),
+        title: _t('app.public.tour.welcome.machines.title'),
+        content: _t('app.public.tour.welcome.machines.content'),
         placement: 'right'
       });
       uitour.createStep({
         selector: '.nav-primary li.reserve-training-link',
         stepId: 'trainings',
         order: 3,
-        title: _t('app.public.tour.trainings.title'),
-        content: _t('app.public.tour.trainings.content'),
+        title: _t('app.public.tour.welcome.trainings.title'),
+        content: _t('app.public.tour.welcome.trainings.content'),
         placement: 'right'
       });
       if (!Fablab.withoutSpaces) {
@@ -116,8 +116,8 @@ Application.Controllers.controller('HomeController', ['$scope', '$stateParams', 
           selector: '.nav-primary li.reserve-space-link',
           stepId: 'spaces',
           order: 4,
-          title: _t('app.public.tour.spaces.title'),
-          content: _t('app.public.tour.spaces.content'),
+          title: _t('app.public.tour.welcome.spaces.title'),
+          content: _t('app.public.tour.welcome.spaces.content'),
           placement: 'right'
         });
       }
@@ -125,72 +125,72 @@ Application.Controllers.controller('HomeController', ['$scope', '$stateParams', 
         selector: '.nav-primary li.reserve-event-link',
         stepId: 'events',
         order: 5,
-        title: _t('app.public.tour.events.title'),
-        content: _t('app.public.tour.events.content'),
+        title: _t('app.public.tour.welcome.events.title'),
+        content: _t('app.public.tour.welcome.events.content'),
         placement: 'right'
       });
       uitour.createStep({
         selector: '.nav-primary li.public-calendar-link',
         stepId: 'calendar',
         order: 6,
-        title: _t('app.public.tour.calendar.title'),
-        content: _t('app.public.tour.calendar.content'),
+        title: _t('app.public.tour.welcome.calendar.title'),
+        content: _t('app.public.tour.welcome.calendar.content'),
         placement: 'right'
       });
       uitour.createStep({
         selector: '.nav-primary li.projects-gallery-link',
         stepId: 'projects',
         order: 7,
-        title: _t('app.public.tour.projects.title'),
-        content: _t('app.public.tour.projects.content'),
+        title: _t('app.public.tour.welcome.projects.title'),
+        content: _t('app.public.tour.welcome.projects.content'),
         placement: 'right'
       });
       uitour.createStep({
         selector: '.nav-primary li.plans-link',
         stepId: 'plans',
         order: 8,
-        title: _t('app.public.tour.plans.title'),
-        content: _t('app.public.tour.plans.content'),
+        title: _t('app.public.tour.welcome.plans.title'),
+        content: _t('app.public.tour.welcome.plans.content'),
         placement: 'right'
       });
       uitour.createStep({
         selector: '.nav-primary .admin-section',
         stepId: 'admin',
         order: 9,
-        title: _t('app.public.tour.admin.title'),
-        content: _t('app.public.tour.admin.content'),
+        title: _t('app.public.tour.welcome.admin.title'),
+        content: _t('app.public.tour.welcome.admin.content'),
         placement: 'right'
       });
       uitour.createStep({
         selector: '.navbar.header li.about-page-link',
         stepId: 'about',
         order: 10,
-        title: _t('app.public.tour.about.title'),
-        content: _t('app.public.tour.about.content'),
+        title: _t('app.public.tour.welcome.about.title'),
+        content: _t('app.public.tour.welcome.about.content'),
         placement: 'bottom'
       });
       uitour.createStep({
         selector: '.navbar.header li.notification-center-link',
         stepId: 'notifications',
         order: 11,
-        title: _t('app.public.tour.notifications.title'),
-        content: _t('app.public.tour.notifications.content'),
+        title: _t('app.public.tour.welcome.notifications.title'),
+        content: _t('app.public.tour.welcome.notifications.content'),
         placement: 'bottom'
       });
       uitour.createStep({
         selector: '.navbar.header li.user-menu-dropdown',
         stepId: 'profile',
         order: 12,
-        title: _t('app.public.tour.profile.title'),
-        content: _t('app.public.tour.profile.content'),
+        title: _t('app.public.tour.welcome.profile.title'),
+        content: _t('app.public.tour.welcome.profile.content'),
         placement: 'bottom'
       });
       uitour.createStep({
         selector: '.app-generator .app-version',
         stepId: 'version',
         order: 13,
-        title: _t('app.public.tour.version.title'),
-        content: _t('app.public.tour.version.content'),
+        title: _t('app.public.tour.welcome.version.title'),
+        content: _t('app.public.tour.welcome.version.content'),
         placement: 'top'
       });
       uitour.createStep({
