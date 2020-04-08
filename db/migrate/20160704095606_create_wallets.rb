422e3c62360dfd01237572425c9705b3a2365ec3
Sylvain <sylvain@sbx.pm>
2020-04-08 15:00:22 +0200

    [bug] migration 20160704095606 cannot run due to GDPR refactoring

--- db/migrate/20160704095606_create_wallets.rb
+++ db/migrate/20160704095606_create_wallets.rb
@@ -9,9 +9,12 @@ class CreateWallets < ActiveRecord::Migration[4.2]
       t.timestamps null: false
     end
 
-    User.includes(:invoicing_profile).each do |u|
-      Wallet.create(invoicing_profile: u.invoicing_profile.id)
-    end
+    # create all wallets
+    execute <<-SQL
+      INSERT INTO wallets (user, amount, created_at, updated_at)
+      SELECT users.id, 0, #{DateTime.current.iso8601}, #{DateTime.current.iso8601}
+      FROM users
+    SQL
   end
 
   def down
