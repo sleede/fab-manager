abcf17d1e64685bf8f9323a1831eae76dd2f8c93
Sylvain <sylvain@sbx.pm>
2020-04-28 16:49:05 +0200

    invoices interface for managers

--- app/policies/accounting_period_policy.rb
+++ app/policies/accounting_period_policy.rb
@@ -2,9 +2,13 @@
 
 # Check the access policies for API::AccountingPeriodsController
 class AccountingPeriodPolicy < ApplicationPolicy
-  %w[index show create last_closing_end download_archive].each do |action|
+  %w[index show create download_archive].each do |action|
     define_method "#{action}?" do
       user.admin?
     end
+  end
+
+  def last_closing_end?
+    user.admin? || user.manager?
   end
 end
--- app/policies/invoice_policy.rb
+++ app/policies/invoice_policy.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
+# Check the access policies for API::InvoicesController
 class InvoicePolicy < ApplicationPolicy
   def index?
     user.admin?
   end
 
   def download?
-    user.admin? or (record.invoicing_profile.user_id == user.id)
+    user.admin? || user.manager? || (record.invoicing_profile.user_id == user.id)
   end
 
   def create?
-    user.admin?
+    user.admin? || user.manager?
   end
 
   def list?
