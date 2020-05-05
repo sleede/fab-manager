0609f444b0bd6b972d738431462d92ebb0e1285a
Sylvain <sylvain@sbx.pm>
2020-05-05 12:37:13 +0200

    corrected ProjectIndexerWorker

--- app/workers/project_indexer_worker.rb
+++ app/workers/project_indexer_worker.rb
@@ -1,20 +1,23 @@
+# frozen_string_literal: true
+
+# Index the projects to ElasticSearch
 class ProjectIndexerWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'elasticsearch', retry: true
 
-  Logger = Sidekiq.logger.level == Logger::DEBUG ? Sidekiq.logger : nil
-  Client = Elasticsearch::Model.client
-
   def perform(operation, record_id)
+    logger = Sidekiq.logger.level == Logger::DEBUG ? Sidekiq.logger : nil
+    client = Elasticsearch::Model.client
+
     logger.debug [operation, "ID: #{record_id}"]
 
     case operation.to_s
-      when /index/
-        record = Project.find(record_id)
-        Client.index  index: Project.index_name, type: Project.document_type, id: record.id, body: record.as_indexed_json
-      when /delete/
-        Client.delete index: Project.index_name, type: Project.document_type, id: record_id
-      else raise ArgumentError, "Unknown operation '#{operation}'"
+    when /index/
+      record = Project.find(record_id)
+      client.index  index: Project.index_name, type: Project.document_type, id: record.id, body: record.as_indexed_json
+    when /delete/
+      client.delete index: Project.index_name, type: Project.document_type, id: record_id
+    else raise ArgumentError, "Unknown operation '#{operation}'"
     end
   end
 end
