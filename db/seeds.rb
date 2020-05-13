2c17cfafedf12bb5a2a5ad28c25549164adcab35
Sylvain <sylvain@sbx.pm>
2020-05-13 15:54:25 +0200

    initialize phoneRequired from env

--- db/seeds.rb
+++ db/seeds.rb
@@ -927,7 +927,7 @@ end
 
 unless Setting.find_by(name: 'phone_required').try(:value)
   setting = Setting.find_or_initialize_by(name: 'phone_required')
-  setting.value = 'true'
+  setting.value = ENV.fetch('PHONE_REQUIRED', 'true')
   setting.save
 end
 
