18e2b032ae8498abad386f08394573135d9a7740
Sylvain <sylvain@sbx.pm>
2020-05-19 11:45:51 +0200

    fix import users from csv

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
-  rolify after_add: :update_statistic_profile, after_remove: :update_statistic_profile
+  rolify
 
   # enable OmniAuth authentication only if needed
   devise :omniauthable, omniauth_providers: [AuthProvider.active.strategy_name.to_sym] unless
@@ -319,6 +319,16 @@ class User < ApplicationRecord
         .delete_if { |col| blacklist.include?(col[0]) }
   end
 
+  # will update the statistic_profile after a group switch or a role update
+  def update_statistic_profile
+    raise NoProfileError if statistic_profile.nil? || statistic_profile.id.nil?
+
+    statistic_profile.update_attributes(
+      group_id: group_id,
+      role_id: roles.first.id
+    )
+  end
+
   protected
 
   # remove projects drafts that are not linked to another user
@@ -428,17 +438,6 @@ class User < ApplicationRecord
 
     invoicing_profile.update_attributes(
       email: email
-    )
-  end
-
-  # will update the statistic_profile after a group switch
-  # Rolify callbacks will call this function with an argument unused here
-  def update_statistic_profile(_param = nil)
-    raise NoProfileError if statistic_profile.nil?
-
-    statistic_profile.update_attributes(
-      group_id: group_id,
-      role_id: roles.first.id
     )
   end
 end
