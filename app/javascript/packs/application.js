75aa90070c254d467838b1a72beb7aaaba84c0bc
Sylvain <sylvain@sleede.com>
2020-09-09 08:45:02 +0200

    moved javascript from sprocket to webpacker

--- app/javascript/packs/application.js
+++
@@ -1,7 +0,0 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
-import '../stylesheets/application.scss.erb';
-
-const images = require.context('../images', true);
-const imagePath = (name) => images(name, true);
