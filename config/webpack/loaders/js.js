562fb5d8d761bc9afb2700a1dd1da1b8b67668c2
Sylvain <sylvain@sleede.com>
2020-09-22 16:03:18 +0200

    fix requiring templates from ngtemplate-loader

--- config/webpack/loaders/js.js
+++ config/webpack/loaders/js.js
@@ -3,10 +3,7 @@ module.exports = {
   exclude: /node_modules/,
   use: [
     {
-      loader: 'auto-ngtemplate-loader',
-      options: {
-        pathResolver: (p) => p.replace(/src\/javascript\/.*$/, 'templates/')
-      }
+      loader: 'auto-ngtemplate-loader'
     }
   ]
 };
