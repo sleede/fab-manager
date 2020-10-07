0d28da1e4490eb058fa5640707dbfb66a059186a
Sylvain <sylvain@sleede.com>
2020-10-07 10:34:27 +0200

    re-enable minifying in production + use fork w/ dependencies injection for angular-ui-codemirror

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,7 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const environment = require('./environment');
-const config = environment.toWebpackConfig();
-delete config.optimization.minimizer;
 
-module.exports = config;
+module.exports = environment.toWebpackConfig();
