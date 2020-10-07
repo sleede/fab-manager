ac4dcfd5fd5b47898e9356f80ca6879ab3afb713
Sylvain <sylvain@sleede.com>
2020-10-07 09:06:36 +0200

    disable uglify in production to find the unknown provider

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const environment = require('./environment');
+const config = environment.toWebpackConfig();
+delete config.optimization.minimizer;
 
-module.exports = environment.toWebpackConfig();
+module.exports = config;
