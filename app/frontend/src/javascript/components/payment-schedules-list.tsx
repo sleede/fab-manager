5e06de508dea2edcc65e72dfc03c0acb61f6ff9f
Sylvain <sylvain@sleede.com>
2021-01-26 11:37:05 +0100

    interface to filter list of schedules

--- app/frontend/src/javascript/components/payment-schedules-list.tsx
+++ app/frontend/src/javascript/components/payment-schedules-list.tsx
@@ -8,6 +8,7 @@ import { useTranslation } from 'react-i18next';
 import { Loader } from './loader';
 import { react2angular } from 'react2angular';
 import PaymentScheduleAPI from '../api/payment-schedule';
+import { LabelledInput } from './labelled-input';
 
 declare var Application: IApplication;
 
@@ -15,13 +16,37 @@ const paymentSchedulesList = PaymentScheduleAPI.list({ query: { page: 1, size: 2
 
 const PaymentSchedulesList: React.FC = () => {
   const { t } = useTranslation('admin');
+  const [referenceFilter, setReferenceFilter] = useState('');
+  const [customerFilter, setCustomerFilter] = useState('');
+  const [dateFilter, setDateFilter] = useState(null);
 
   const paymentSchedules = paymentSchedulesList.read();
 
   return (
     <div className="payment-schedules-list">
+      <h3>
+        <i className="fas fa-filter" />
+        {t('app.admin.invoices.payment_schedules.filter_schedules')}
+      </h3>
+      <div className="schedules-filters">
+        <LabelledInput id="reference"
+                       label={t('app.admin.invoices.payment_schedules.reference')}
+                       type="text"
+                       onChange={setReferenceFilter}
+                       value={referenceFilter} />
+        <LabelledInput id="customer"
+                       label={t('app.admin.invoices.payment_schedules.customer')}
+                       type="text"
+                       onChange={setCustomerFilter}
+                       value={customerFilter} />
+        <LabelledInput id="reference"
+                       label={t('app.admin.invoices.payment_schedules.date')}
+                       type="date"
+                       onChange={setDateFilter}
+                       value={dateFilter} />
+      </div>
       <ul>
-      {paymentSchedules.map(p => `<li>${p.reference}</li>`)}
+        {paymentSchedules.map(p => <li>{p.reference}</li>)}
       </ul>
     </div>
   );
