e3fa75dd6e8d213d6424e0bdca1ace8e4ca5bdf3
Sylvain <sylvain@sleede.com>
2021-11-10 17:02:37 +0100

    [bug] unable to create a plan

---
+++ test/integration/plans/create_plan_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+
+class CreatePlanTest < Minitest::Test
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    skip 'Not implemented'
+  end
+end
