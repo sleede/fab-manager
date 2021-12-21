6091cec82eb5b6b001bde258a2360e4123e5f65d
Sylvain <sylvain@sleede.com>
2021-12-21 14:18:03 +0100

    api endpoint to create prices

--- app/controllers/api/prices_controller.rb
+++ app/controllers/api/prices_controller.rb
@@ -5,6 +5,19 @@
 class API::PricesController < API::ApiController
   before_action :authenticate_user!
 
+  def create
+    @price = Price.new(price_params)
+    @price.amount *= 100
+
+    authorize @price
+
+    if @price.save
+      render json: @price, status: :created
+    else
+      render json: @price.errors, status: :unprocessable_entity
+    end
+  end
+
   def index
     @prices = PriceService.list(params)
   end
@@ -28,6 +41,10 @@ class API::PricesController < API::ApiController
   end
 
   private
+
+  def price_create_params
+    params.require(:price).permit(:amount, :duration, :group_id, :plan_id, :priceable_id, :priceable_type)
+  end
 
   def price_params
     params.require(:price).permit(:amount, :duration)
