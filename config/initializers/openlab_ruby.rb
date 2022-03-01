d6f5980965089a56fcb72569be06ebcb1984cdb3
Du Peng <gnepud@gmail.com>
2022-03-01 18:42:57 +0100

    Fix a bug: unable to modify OpenLab url for production

--- config/initializers/openlab_ruby.rb
+++ config/initializers/openlab_ruby.rb
@@ -1,3 +1,3 @@
 Openlab.configure do |config|
-  config.base_uri = Rails.application.secrets.openlab_base_uri unless Rails.env.production?
+  config.base_uri = Rails.application.secrets.openlab_base_uri if Rails.application.secrets.openlab_base_uri
 end
