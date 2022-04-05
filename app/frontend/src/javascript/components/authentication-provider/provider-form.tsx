4bf05a5024f0a5dd72a03058b37a1c571b308c6b
Sylvain <sylvain@sleede.com>
2022-04-05 11:43:28 +0200

    (ui) select authentication provier type

--- app/frontend/src/javascript/components/authentication-provider/provider-form.tsx
+++ app/frontend/src/javascript/components/authentication-provider/provider-form.tsx
@@ -6,8 +6,16 @@ import { Loader } from '../base/loader';
 import { IApplication } from '../../models/application';
 import { RHFInput } from '../base/rhf-input';
 import { useTranslation } from 'react-i18next';
+import { FabSelect } from '../base/fab-select';
 
 declare const Application: IApplication;
+
+// list of supported authentication methods
+const METHODS = {
+  DatabaseProvider: 'local_database',
+  OAuth2Provider: 'o_auth2',
+  OpenIdConnectProvider: 'openid_connect'
+};
 
 interface ProviderFormProps {
   action: 'create' | 'update',
@@ -16,8 +24,10 @@ interface ProviderFormProps {
   onSuccess: (message: string) => void,
 }
 
+type selectProvidableTypeOption = { value: string, label: string };
+
 export const ProviderForm: React.FC<ProviderFormProps> = ({ action, provider, onError, onSuccess }) => {
-  const { handleSubmit, register } = useForm<AuthenticationProvider>({ defaultValues: { ...provider } });
+  const { handleSubmit, register, control } = useForm<AuthenticationProvider>({ defaultValues: { ...provider } });
   const { t } = useTranslation('shared');
 
   const onSubmit: SubmitHandler<AuthenticationProvider> = (data: AuthenticationProvider) => {
@@ -28,9 +38,17 @@ export const ProviderForm: React.FC<ProviderFormProps> = ({ action, provider, on
     }
   };
 
+  const buildProvidableTypeOptions = (): Array<selectProvidableTypeOption> => {
+    return Object.keys(METHODS).map((method: string) => {
+      return { value: method, label: t(`app.shared.authentication.${METHODS[method]}`) };
+    });
+  };
+
   return (
     <form className="provider-form" onSubmit={handleSubmit(onSubmit)}>
       <RHFInput id="name" register={register} readOnly={action === 'update'} rules={{ required: true }} label={t('app.shared.authentication.name')} />
+      <FabSelect id="providable_type" control={control} options={buildProvidableTypeOptions()} valueDefault={'DatabaseProvider'} />
+      <input type={'submit'} />
     </form>
   );
 };
