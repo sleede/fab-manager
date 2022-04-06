2baee48eafbae48023de2e09e1a2e454f3059704
Sylvain <sylvain@sleede.com>
2022-04-06 12:24:04 +0200

    (ui) form-select: onChange cb

--- app/frontend/src/javascript/components/form/form-select.tsx
+++ app/frontend/src/javascript/components/form/form-select.tsx
@@ -1,4 +1,4 @@
-import React, { SelectHTMLAttributes } from 'react';
+import React from 'react';
 import Select from 'react-select';
 import { Controller, Path } from 'react-hook-form';
 import { FieldValues } from 'react-hook-form/dist/types/fields';
@@ -6,11 +6,15 @@ import { FieldPath } from 'react-hook-form/dist/types/path';
 import { FieldPathValue, UnpackNestedValue } from 'react-hook-form/dist/types';
 import { FormControlledComponent } from '../../models/form-component';
 
-interface FormSelectProps<TFieldValues, TContext extends object, TOptionValue> extends SelectHTMLAttributes<HTMLSelectElement>, FormControlledComponent<TFieldValues, TContext> {
+interface FormSelectProps<TFieldValues, TContext extends object, TOptionValue> extends FormControlledComponent<TFieldValues, TContext> {
   id: string,
   label?: string,
   options: Array<selectOption<TOptionValue>>,
   valueDefault?: TOptionValue,
+  onChange?: (value: TOptionValue) => void,
+  className?: string,
+  placeholder?: string,
+  disabled?: boolean,
 }
 
 /**
@@ -22,12 +26,22 @@ type selectOption<TOptionValue> = { value: TOptionValue, label: string };
 /**
  * This component is a wrapper for react-select to use with react-hook-form
  */
-export const FormSelect = <TFieldValues extends FieldValues, TContext extends object, TOptionValue>({ id, label, className, control, placeholder, options, valueDefault, error, rules, disabled }: FormSelectProps<TFieldValues, TContext, TOptionValue>) => {
+export const FormSelect = <TFieldValues extends FieldValues, TContext extends object, TOptionValue>({ id, label, className, control, placeholder, options, valueDefault, error, rules, disabled, onChange }: FormSelectProps<TFieldValues, TContext, TOptionValue>) => {
   const classNames = `
     form-item ${className || ''}
     ${error && error[id] ? 'is-incorrect' : ''}
     ${rules && rules.required ? 'is-required' : ''}
     ${disabled ? 'is-disabled' : ''}`;
+
+  /**
+   * The following callback will trigger the onChange callback, if it was passed to this component,
+   * when the selected option changes.
+   */
+  const onChangeCb = (newValue: TOptionValue): void => {
+    if (typeof onChange === 'function') {
+      onChange(newValue);
+    }
+  };
 
   return (
     <label className={classNames}>
@@ -39,14 +53,17 @@ export const FormSelect = <TFieldValues extends FieldValues, TContext extends ob
                     control={control}
                     defaultValue={valueDefault as UnpackNestedValue<FieldPathValue<TFieldValues, Path<TFieldValues>>>}
                     render={({ field: { onChange, value, ref } }) =>
-          <Select inputRef={ref}
-                  classNamePrefix="rs"
-                  className="rs"
-                  value={options.find(c => c.value === value)}
-                  onChange={val => onChange(val.value)}
-                  placeholder={placeholder}
-                  options={options} />
-        } />
+                      <Select ref={ref}
+                              classNamePrefix="rs"
+                              className="rs"
+                              value={options.find(c => c.value === value)}
+                              onChange={val => {
+                                onChangeCb(val.value);
+                                onChange(val.value);
+                              }}
+                              placeholder={placeholder}
+                              options={options} />
+                    } />
       </div>
     </label>
   );
