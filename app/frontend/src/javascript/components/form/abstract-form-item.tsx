03ed350e69d939157b372de541cb4684872f741e
Sylvain <sylvain@sleede.com>
2022-04-27 12:55:43 +0200

    (ui) form-switch component
    + refactored form components

---
+++ app/frontend/src/javascript/components/form/abstract-form-item.tsx
@@ -0,0 +1,59 @@
+import React, { PropsWithChildren, ReactNode, useEffect, useState } from 'react';
+import { AbstractFormComponent } from '../../models/form-component';
+import { FieldValues } from 'react-hook-form/dist/types/fields';
+import { get as _get } from 'lodash';
+
+export interface AbstractFormItemProps<TFieldValues> extends PropsWithChildren<AbstractFormComponent<TFieldValues>> {
+  id: string,
+  label?: string,
+  tooltip?: ReactNode,
+  className?: string,
+  disabled?: boolean,
+  readOnly?: boolean
+}
+
+/**
+ * This abstract component should not be used directly.
+ * Other forms components that are intended to be used with react-hook-form must extend this component.
+ */
+export const AbstractFormItem = <TFieldValues extends FieldValues>({ id, label, tooltip, className, disabled, readOnly, error, warning, rules, formState, children }: AbstractFormItemProps<TFieldValues>) => {
+  const [isDirty, setIsDirty] = useState(false);
+  const [fieldError, setFieldError] = useState(error);
+
+  useEffect(() => {
+    setIsDirty(_get(formState?.dirtyFields, id));
+    setFieldError(_get(formState?.errors, id));
+  }, [formState]);
+
+  useEffect(() => {
+    setFieldError(error);
+  }, [error]);
+
+  // Compose classnames from props
+  const classNames = [
+    'form-item',
+    `${className || ''}`,
+    `${isDirty && fieldError ? 'is-incorrect' : ''}`,
+    `${isDirty && warning ? 'is-warned' : ''}`,
+    `${rules && rules.required ? 'is-required' : ''}`,
+    `${readOnly ? 'is-readonly' : ''}`,
+    `${disabled ? 'is-disabled' : ''}`
+  ].join(' ');
+
+  return (
+    <label className={classNames}>
+      {label && <div className='form-item-header'>
+        <p>{label}</p>
+        {tooltip && <div className="item-tooltip">
+          <span className="trigger"><i className="fa fa-question-circle" /></span>
+          <div className="content">{tooltip}</div>
+        </div>}
+      </div>}
+      <div className='form-item-field'>
+        {children}
+      </div>
+      {(isDirty && fieldError) && <div className="form-item-error">{fieldError.message}</div> }
+      {(isDirty && warning) && <div className="form-item-warning">{warning.message}</div> }
+    </label>
+  );
+};
--- app/frontend/src/javascript/models/form-component.ts
+++ app/frontend/src/javascript/models/form-component.ts
@@ -16,18 +16,17 @@ export type ruleTypes = {
  * Automatic error handling is done through the `formState` prop.
  * Even for manual error/warning, the `formState` prop is required, because it is used to determine is the field is dirty.
  */
-export interface FormComponent<TFieldValues> {
-  register: UseFormRegister<TFieldValues>,
+export interface AbstractFormComponent<TFieldValues> {
   error?: { message: string },
   warning?: { message: string },
   rules?: ruleTypes,
   formState?: FormState<TFieldValues>;
 }
 
-export interface FormControlledComponent<TFieldValues, TContext extends object> {
-  control: Control<TFieldValues, TContext>,
-  error?: { message: string },
-  warning?: { message: string },
-  rules?: ruleTypes,
-  formState?: FormState<TFieldValues>;
+export interface FormComponent<TFieldValues> extends AbstractFormComponent<TFieldValues> {
+  register: UseFormRegister<TFieldValues>,
+}
+
+export interface FormControlledComponent<TFieldValues, TContext extends object> extends AbstractFormComponent<TFieldValues> {
+  control: Control<TFieldValues, TContext>
 }
