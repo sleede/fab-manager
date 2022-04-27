18a80a225c942ad973a4392e3d63f9a6f5f76314
Sylvain <sylvain@sleede.com>
2022-04-27 16:37:37 +0200

    (bug) define uid field in openid connect configuration

--- app/services/auth_provider_service.rb
+++ app/services/auth_provider_service.rb
@@ -20,6 +20,9 @@ class AuthProviderService
       provider.providable.display = 'page'
       provider.providable.response_mode = 'query'
       provider.providable.response_type = 'code'
+      provider.providable.uid_field = provider.auth_provider_mappings
+                                              .find { |m| m.local_model == 'user' && m.local_field == 'uid' }
+                                              .api_field
 
       URI.parse(provider.providable.issuer).tap do |uri|
         provider.providable.client__scheme = uri.scheme
