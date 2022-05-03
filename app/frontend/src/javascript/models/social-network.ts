cb5dd953ea61df754bb4378dc301e74f77cef2a3
vincent <vincent@sleede.com>
2022-05-03 18:05:30 +0200

    Edit and display fab's social networks

--- app/frontend/src/javascript/models/social-network.ts
+++ app/frontend/src/javascript/models/social-network.ts
@@ -2,3 +2,19 @@ export interface SocialNetwork {
   name: string,
   url: string
 }
+
+export const supportedNetworks = [
+  'facebook',
+  'twitter',
+  'viadeo',
+  'linkedin',
+  'instagram',
+  'youtube',
+  'vimeo',
+  'dailymotion',
+  'github',
+  'echosciences',
+  'pinterest',
+  'lastfm',
+  'flickr'
+];
