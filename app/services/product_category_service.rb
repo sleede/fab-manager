e096d95dcc7c71e5cde68aeb47f5ea2be767b698
Du Peng <gnepud@gmail.com>
2022-07-25 11:16:41 +0200

    reset product_category_id to nil if product_category is removed

--- app/services/product_category_service.rb
+++ app/services/product_category_service.rb
@@ -7,7 +7,13 @@ class ProductCategoryService
   end
 
   def self.destroy(product_category)
-    ProductCategory.where(parent_id: product_category.id).destroy_all
-    product_category.destroy
+    ActiveRecord::Base.transaction do
+      sub_categories = ProductCategory.where(parent_id: product_category.id)
+      # remove product_category and sub-categories related id in product
+      Product.where(product_category_id: sub_categories.map(&:id).push(product_category.id)).update(product_category_id: nil)
+      # remove all sub-categories
+      sub_categories.destroy_all
+      product_category.destroy
+    end
   end
 end
