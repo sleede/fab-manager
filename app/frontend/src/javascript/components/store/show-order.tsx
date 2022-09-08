d5d8a972cfc19e5869d4d9e93fffcabb12e1a24d
vincent <vincent@sleede.com>
2022-09-08 12:04:37 +0200

    (quality) Refacto orders views

--- app/frontend/src/javascript/components/store/show-order.tsx
+++ app/frontend/src/javascript/components/store/show-order.tsx
@@ -1,7 +1,7 @@
-/* eslint-disable fabmanager/scoped-translation */
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { IApplication } from '../../models/application';
+import { User } from '../../models/user';
 import { react2angular } from 'react2angular';
 import { Loader } from '../base/loader';
 import noImage from '../../../../images/no_image.png';
@@ -12,6 +12,7 @@ declare const Application: IApplication;
 
 interface ShowOrderProps {
   orderRef: string,
+  currentUser?: User,
   onError: (message: string) => void,
   onSuccess: (message: string) => void
 }
@@ -26,22 +27,29 @@ type selectOption = { value: number, label: string };
  */
 // TODO: delete next eslint disable
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const ShowOrder: React.FC<ShowOrderProps> = ({ orderRef, onError, onSuccess }) => {
-  const { t } = useTranslation('admin');
+export const ShowOrder: React.FC<ShowOrderProps> = ({ orderRef, currentUser, onError, onSuccess }) => {
+  const { t } = useTranslation('shared');
+
+  /**
+   * Check if the current operator has administrative rights or is a normal member
+   */
+  const isPrivileged = (): boolean => {
+    return (currentUser?.role === 'admin' || currentUser?.role === 'manager');
+  };
 
   /**
    * Creates sorting options to the react-select format
    */
   const buildOptions = (): Array<selectOption> => {
     return [
-      { value: 0, label: t('app.admin.store.orders.status.error') },
-      { value: 1, label: t('app.admin.store.orders.status.canceled') },
-      { value: 2, label: t('app.admin.store.orders.status.pending') },
-      { value: 3, label: t('app.admin.store.orders.status.under_preparation') },
-      { value: 4, label: t('app.admin.store.orders.status.paid') },
-      { value: 5, label: t('app.admin.store.orders.status.ready') },
-      { value: 6, label: t('app.admin.store.orders.status.collected') },
-      { value: 7, label: t('app.admin.store.orders.status.refunded') }
+      { value: 0, label: t('app.shared.store.show_order.status.error') },
+      { value: 1, label: t('app.shared.store.show_order.status.canceled') },
+      { value: 2, label: t('app.shared.store.show_order.status.pending') },
+      { value: 3, label: t('app.shared.store.show_order.status.under_preparation') },
+      { value: 4, label: t('app.shared.store.show_order.status.paid') },
+      { value: 5, label: t('app.shared.store.show_order.status.ready') },
+      { value: 6, label: t('app.shared.store.show_order.status.collected') },
+      { value: 7, label: t('app.shared.store.show_order.status.refunded') }
     ];
   };
 
@@ -85,33 +93,37 @@ export const ShowOrder: React.FC<ShowOrderProps> = ({ orderRef, onError, onSucce
       <header>
         <h2>[order.ref]</h2>
         <div className="grpBtn">
-          <Select
-            options={buildOptions()}
-            onChange={option => handleAction(option)}
-            styles={customStyles}
-          />
+          {isPrivileged() &&
+            <Select
+              options={buildOptions()}
+              onChange={option => handleAction(option)}
+              styles={customStyles}
+            />
+          }
           <a href={''}
             target='_blank'
             className='fab-button is-black'
             rel='noreferrer'>
-            {t('app.admin.store.show_order.see_invoice')}
+            {t('app.shared.store.show_order.see_invoice')}
           </a>
         </div>
       </header>
 
       <div className="client-info">
-        <label>{t('app.admin.store.show_order.client')}</label>
+        <label>{t('app.shared.store.show_order.tracking')}</label>
         <div className="content">
+          {isPrivileged() &&
+            <div className='group'>
+              <span>{t('app.shared.store.show_order.client')}</span>
+              <p>order.user.name</p>
+            </div>
+          }
           <div className='group'>
-            <span>{t('app.admin.store.show_order.client')}</span>
-            <p>order.user.name</p>
-          </div>
-          <div className='group'>
-            <span>{t('app.admin.store.show_order.created_at')}</span>
+            <span>{t('app.shared.store.show_order.created_at')}</span>
             <p>order.created_at</p>
           </div>
           <div className='group'>
-            <span>{t('app.admin.store.show_order.last_update')}</span>
+            <span>{t('app.shared.store.show_order.last_update')}</span>
             <p>order.???</p>
           </div>
           <FabStateLabel status={statusColor('error')} background>
@@ -121,7 +133,7 @@ export const ShowOrder: React.FC<ShowOrderProps> = ({ orderRef, onError, onSucce
       </div>
 
       <div className="cart">
-        <label>{t('app.admin.store.show_order.cart')}</label>
+        <label>{t('app.shared.store.show_order.cart')}</label>
         <div>
           {/* loop sur les articles du panier */}
           <article className='store-cart-list-item'>
@@ -129,19 +141,19 @@ export const ShowOrder: React.FC<ShowOrderProps> = ({ orderRef, onError, onSucce
               <img alt=''src={noImage} />
             </div>
             <div className="ref">
-              <span>{t('app.admin.store.show_order.reference_short')} orderable_id?</span>
+              <span>{t('app.shared.store.show_order.reference_short')} orderable_id?</span>
               <p>o.orderable_name</p>
             </div>
             <div className="actions">
               <div className='price'>
                 <p>o.amount</p>
-                <span>/ {t('app.admin.store.show_order.unit')}</span>
+                <span>/ {t('app.shared.store.show_order.unit')}</span>
               </div>
 
               <span className="count">o.quantity</span>
 
               <div className='total'>
-                <span>{t('app.admin.store.show_order.item_total')}</span>
+                <span>{t('app.shared.store.show_order.item_total')}</span>
                 <p>o.quantity * o.amount</p>
               </div>
             </div>
@@ -151,15 +163,15 @@ export const ShowOrder: React.FC<ShowOrderProps> = ({ orderRef, onError, onSucce
 
       <div className="subgrid">
         <div className="payment-info">
-          <label>{t('app.admin.store.show_order.payment_informations')}</label>
+          <label>{t('app.shared.store.show_order.payment_informations')}</label>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rerum commodi quaerat possimus! Odit, harum.</p>
         </div>
         <div className="amount">
-          <label>{t('app.admin.store.show_order.amount')}</label>
-          <p>{t('app.admin.store.show_order.products_total')}<span>order.amount</span></p>
-          <p className='gift'>{t('app.admin.store.show_order.gift_total')}<span>-order.amount</span></p>
-          <p>{t('app.admin.store.show_order.coupon')}<span>order.amount</span></p>
-          <p className='total'>{t('app.admin.store.show_order.total')} <span>order.total</span></p>
+          <label>{t('app.shared.store.show_order.amount')}</label>
+          <p>{t('app.shared.store.show_order.products_total')}<span>order.amount</span></p>
+          <p className='gift'>{t('app.shared.store.show_order.gift_total')}<span>-order.amount</span></p>
+          <p>{t('app.shared.store.show_order.coupon')}<span>order.amount</span></p>
+          <p className='total'>{t('app.shared.store.show_order.cart_total')} <span>order.total</span></p>
         </div>
       </div>
     </div>
@@ -174,4 +186,4 @@ const ShowOrderWrapper: React.FC<ShowOrderProps> = (props) => {
   );
 };
 
-Application.Components.component('showOrder', react2angular(ShowOrderWrapper, ['orderRef', 'onError', 'onSuccess']));
+Application.Components.component('showOrder', react2angular(ShowOrderWrapper, ['orderRef', 'currentUser', 'onError', 'onSuccess']));
