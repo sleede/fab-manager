c9901a89e1e7c87c2d7a3b7b76bc31892abdfd74
Sylvain <sylvain@sleede.com>
2022-10-25 14:31:31 +0200

    (test) test admin reserves for himself

--- app/policies/local_payment_policy.rb
+++ app/policies/local_payment_policy.rb
@@ -6,6 +6,6 @@ class LocalPaymentPolicy < ApplicationPolicy
     # only admins and managers can offer free extensions of a subscription
     has_free_days = record.shopping_cart.items.any? { |item| item.is_a? CartItem::FreeExtension }
 
-    user.admin? || (user.manager? && record.shopping_cart.customer.id != user.id) || (record.price.zero? && !has_free_days)
+    ((user.admin? || user.manager?) && record.shopping_cart.customer.id != user.id) || (record.price.zero? && !has_free_days)
   end
 end
