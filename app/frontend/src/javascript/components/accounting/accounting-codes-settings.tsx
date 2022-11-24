13eeff59644f4340fb24a39932d2cc88998f6735
Sylvain <sylvain@sleede.com>
2022-11-24 10:56:15 +0100

    (ui) enable advanced accounting button in separate box

--- app/frontend/src/javascript/components/accounting/accounting-codes-settings.tsx
+++ app/frontend/src/javascript/components/accounting/accounting-codes-settings.tsx
@@ -10,6 +10,7 @@ import { SettingName, SettingValue, accountingSettings } from '../../models/sett
 import SettingAPI from '../../api/setting';
 import SettingLib from '../../lib/setting';
 import { FormSwitch } from '../form/form-switch';
+import { FabPanel } from '../base/fab-panel';
 
 declare const Application: IApplication;
 
@@ -48,84 +49,90 @@ export const AccountingCodesSettings: React.FC<AccountingCodesSettingsProps> = (
 
   return (
     <form className="accounting-codes-settings" onSubmit={handleSubmit(onSubmit)}>
-      <h4>{t('app.admin.accounting_codes_settings.journal_code')}</h4>
-      <FormInput register={register} id="accounting_journal_code" label={t('app.admin.accounting_codes_settings.journal_code')} />
-      <h4>{t('app.admin.accounting_codes_settings.payment_means')}</h4>
-      <h5>{t('app.admin.accounting_codes_settings.card')}</h5>
-      <div className="cards">
-        <FormInput register={register} id="accounting_card_client_code" label={t('app.admin.accounting_codes_settings.code')} />
-        <FormInput register={register} id="accounting_card_client_label" label={t('app.admin.accounting_codes_settings.label')} />
-      </div>
-      <h5>{t('app.admin.accounting_codes_settings.wallet_debit')}</h5>
-      <div className="wallets">
-        <FormInput register={register} id="accounting_wallet_client_code" label={t('app.admin.accounting_codes_settings.code')} />
-        <FormInput register={register} id="accounting_wallet_client_label" label={t('app.admin.accounting_codes_settings.label')} />
-      </div>
-      <h5>{t('app.admin.accounting_codes_settings.other')}</h5>
-      <div className="others">
-        <FormInput register={register} id="accounting_other_client_code" label={t('app.admin.accounting_codes_settings.code')} />
-        <FormInput register={register} id="accounting_other_client_label" label={t('app.admin.accounting_codes_settings.label')} />
-      </div>
-      <h4>{t('app.admin.accounting_codes_settings.sales')}</h4>
-      <h5>{t('app.admin.accounting_codes_settings.subscriptions')}</h5>
-      <div className="subscriptions">
-        <FormInput register={register} id="accounting_subscription_code" label={t('app.admin.accounting_codes_settings.code')} />
-        <FormInput register={register} id="accounting_subscription_label" label={t('app.admin.accounting_codes_settings.label')} />
-      </div>
-      <h5>{t('app.admin.accounting_codes_settings.machine')}</h5>
-      <div className="machine">
-        <FormInput register={register} id="accounting_Machine_code" label={t('app.admin.accounting_codes_settings.code')} />
-        <FormInput register={register} id="accounting_Machine_label" label={t('app.admin.accounting_codes_settings.label')} />
-      </div>
-      <h5>{t('app.admin.accounting_codes_settings.training')}</h5>
-      <div className="training">
-        <FormInput register={register} id="accounting_Training_code" label={t('app.admin.accounting_codes_settings.code')} />
-        <FormInput register={register} id="accounting_Training_label" label={t('app.admin.accounting_codes_settings.label')} />
-      </div>
-      <h5>{t('app.admin.accounting_codes_settings.event')}</h5>
-      <div className="events">
-        <FormInput register={register} id="accounting_Event_code" label={t('app.admin.accounting_codes_settings.code')} />
-        <FormInput register={register} id="accounting_Event_label" label={t('app.admin.accounting_codes_settings.label')} />
-      </div>
-      <h5>{t('app.admin.accounting_codes_settings.space')}</h5>
-      <div className="space">
-        <FormInput register={register} id="accounting_Space_code" label={t('app.admin.accounting_codes_settings.code')} />
-        <FormInput register={register} id="accounting_Space_label" label={t('app.admin.accounting_codes_settings.label')} />
-      </div>
-      <h5>{t('app.admin.accounting_codes_settings.prepaid_pack')}</h5>
-      <div className="prepaid_pack">
-        <FormInput register={register} id="accounting_Pack_code" label={t('app.admin.accounting_codes_settings.code')} />
-        <FormInput register={register} id="accounting_Pack_label" label={t('app.admin.accounting_codes_settings.label')} />
-      </div>
-      <h5>{t('app.admin.accounting_codes_settings.product')}</h5>
-      <div className="product">
-        <FormInput register={register} id="accounting_Product_code" label={t('app.admin.accounting_codes_settings.code')} />
-        <FormInput register={register} id="accounting_Product_label" label={t('app.admin.accounting_codes_settings.label')} />
-      </div>
-      <h4>{t('app.admin.accounting_codes_settings.wallet_credit')}</h4>
-      <div className="wallets">
-        <FormInput register={register} id="accounting_wallet_code" label={t('app.admin.accounting_codes_settings.code')} />
-        <FormInput register={register} id="accounting_wallet_label" label={t('app.admin.accounting_codes_settings.label')} />
-      </div>
-      <h4>{t('app.admin.accounting_codes_settings.VAT')}</h4>
-      <div className="vat">
-        <FormInput register={register} id="accounting_VAT_code" label={t('app.admin.accounting_codes_settings.code')} />
-        <FormInput register={register} id="accounting_VAT_label" label={t('app.admin.accounting_codes_settings.label')} />
-      </div>
-      <h4>{t('app.admin.accounting_codes_settings.error')}</h4>
-      <div className="error">
-        <FormInput register={register} id="accounting_Error_code" label={t('app.admin.accounting_codes_settings.code')} />
-        <FormInput register={register} id="accounting_Error_label"
-                   label={t('app.admin.accounting_codes_settings.label')}
-                   tooltip={t('app.admin.accounting_codes_settings.error_help')} />
-      </div>
-      <h4>{t('app.admin.accounting_codes_settings.advanced_accounting')}</h4>
-      <FormSwitch control={control} id="advanced_accounting"
-                  label={t('app.admin.accounting_codes_settings.enable_advanced')}
-                  tooltip={t('app.admin.accounting_codes_settings.enable_advanced_help')} />
-      <FabButton type="submit" className="is-info submit-btn">
-        {t('app.admin.accounting_codes_settings.save')}
-      </FabButton>
+      <FabPanel>
+        <h4>{t('app.admin.accounting_codes_settings.advanced_accounting')}</h4>
+        <FormSwitch control={control} id="advanced_accounting"
+                    label={t('app.admin.accounting_codes_settings.enable_advanced')}
+                    tooltip={t('app.admin.accounting_codes_settings.enable_advanced_help')} />
+      </FabPanel>
+      <FabPanel>
+        <h4>{t('app.admin.accounting_codes_settings.journal_code')}</h4>
+        <FormInput register={register} id="accounting_journal_code" label={t('app.admin.accounting_codes_settings.journal_code')} />
+        <h4>{t('app.admin.accounting_codes_settings.payment_means')}</h4>
+        <h5>{t('app.admin.accounting_codes_settings.card')}</h5>
+        <div className="cards">
+          <FormInput register={register} id="accounting_card_client_code" label={t('app.admin.accounting_codes_settings.code')} />
+          <FormInput register={register} id="accounting_card_client_label" label={t('app.admin.accounting_codes_settings.label')} />
+        </div>
+        <h5>{t('app.admin.accounting_codes_settings.wallet_debit')}</h5>
+        <div className="wallets">
+          <FormInput register={register} id="accounting_wallet_client_code" label={t('app.admin.accounting_codes_settings.code')} />
+          <FormInput register={register} id="accounting_wallet_client_label" label={t('app.admin.accounting_codes_settings.label')} />
+        </div>
+        <h5>{t('app.admin.accounting_codes_settings.other')}</h5>
+        <div className="others">
+          <FormInput register={register} id="accounting_other_client_code" label={t('app.admin.accounting_codes_settings.code')} />
+          <FormInput register={register} id="accounting_other_client_label" label={t('app.admin.accounting_codes_settings.label')} />
+        </div>
+        <h4>{t('app.admin.accounting_codes_settings.sales')}</h4>
+        <h5>{t('app.admin.accounting_codes_settings.subscriptions')}</h5>
+        <div className="subscriptions">
+          <FormInput register={register} id="accounting_subscription_code" label={t('app.admin.accounting_codes_settings.code')} />
+          <FormInput register={register} id="accounting_subscription_label" label={t('app.admin.accounting_codes_settings.label')} />
+        </div>
+        <h5>{t('app.admin.accounting_codes_settings.machine')}</h5>
+        <div className="machine">
+          <FormInput register={register} id="accounting_Machine_code" label={t('app.admin.accounting_codes_settings.code')} />
+          <FormInput register={register} id="accounting_Machine_label" label={t('app.admin.accounting_codes_settings.label')} />
+        </div>
+        <h5>{t('app.admin.accounting_codes_settings.training')}</h5>
+        <div className="training">
+          <FormInput register={register} id="accounting_Training_code" label={t('app.admin.accounting_codes_settings.code')} />
+          <FormInput register={register} id="accounting_Training_label" label={t('app.admin.accounting_codes_settings.label')} />
+        </div>
+        <h5>{t('app.admin.accounting_codes_settings.event')}</h5>
+        <div className="events">
+          <FormInput register={register} id="accounting_Event_code" label={t('app.admin.accounting_codes_settings.code')} />
+          <FormInput register={register} id="accounting_Event_label" label={t('app.admin.accounting_codes_settings.label')} />
+        </div>
+        <h5>{t('app.admin.accounting_codes_settings.space')}</h5>
+        <div className="space">
+          <FormInput register={register} id="accounting_Space_code" label={t('app.admin.accounting_codes_settings.code')} />
+          <FormInput register={register} id="accounting_Space_label" label={t('app.admin.accounting_codes_settings.label')} />
+        </div>
+        <h5>{t('app.admin.accounting_codes_settings.prepaid_pack')}</h5>
+        <div className="prepaid_pack">
+          <FormInput register={register} id="accounting_Pack_code" label={t('app.admin.accounting_codes_settings.code')} />
+          <FormInput register={register} id="accounting_Pack_label" label={t('app.admin.accounting_codes_settings.label')} />
+        </div>
+        <h5>{t('app.admin.accounting_codes_settings.product')}</h5>
+        <div className="product">
+          <FormInput register={register} id="accounting_Product_code" label={t('app.admin.accounting_codes_settings.code')} />
+          <FormInput register={register} id="accounting_Product_label" label={t('app.admin.accounting_codes_settings.label')} />
+        </div>
+        <h4>{t('app.admin.accounting_codes_settings.wallet_credit')}</h4>
+        <div className="wallets">
+          <FormInput register={register} id="accounting_wallet_code" label={t('app.admin.accounting_codes_settings.code')} />
+          <FormInput register={register} id="accounting_wallet_label" label={t('app.admin.accounting_codes_settings.label')} />
+        </div>
+        <h4>{t('app.admin.accounting_codes_settings.VAT')}</h4>
+        <div className="vat">
+          <FormInput register={register} id="accounting_VAT_code" label={t('app.admin.accounting_codes_settings.code')} />
+          <FormInput register={register} id="accounting_VAT_label" label={t('app.admin.accounting_codes_settings.label')} />
+        </div>
+        <h4>{t('app.admin.accounting_codes_settings.error')}</h4>
+        <div className="error">
+          <FormInput register={register} id="accounting_Error_code" label={t('app.admin.accounting_codes_settings.code')} />
+          <FormInput register={register} id="accounting_Error_label"
+                     label={t('app.admin.accounting_codes_settings.label')}
+                     tooltip={t('app.admin.accounting_codes_settings.error_help')} />
+        </div>
+      </FabPanel>
+      <FabPanel className="actions">
+        <FabButton type="submit" className="is-info submit-btn">
+          {t('app.admin.accounting_codes_settings.save')}
+        </FabButton>
+      </FabPanel>
     </form>
   );
 };
