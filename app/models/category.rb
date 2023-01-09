d191712ec0137b337ac7d4681eeda5f2d98f58e4
Sylvain <sylvain@sleede.com>
2023-01-09 09:24:11 +0100

    (bug) unable to update an event category

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,17 +12,15 @@ class Category < ApplicationRecord
   after_update :update_statistic_subtype, if: :saved_change_to_name?
   after_destroy :remove_statistic_subtype
 
-
   def create_statistic_subtype
-    index = StatisticIndex.where(es_type_key: 'event')
-    StatisticSubType.create!(statistic_types: index.first.statistic_types, key: slug, label: name)
+    index = StatisticIndex.find_by(es_type_key: 'event')
+    StatisticSubType.create!(statistic_types: index.statistic_types, key: slug, label: name)
   end
 
   def update_statistic_subtype
-    index = StatisticIndex.where(es_type_key: 'event')
+    index = StatisticIndex.find_by(es_type_key: 'event')
     subtype = StatisticSubType.joins(statistic_type_sub_types: :statistic_type)
-                              .where(key: slug, statistic_types: { statistic_index_id: index.first.id })
-                              .first
+                              .find_by(key: previous_changes[:name][0], statistic_types: { statistic_index_id: index.id })
     subtype.label = name
     subtype.save!
   end
