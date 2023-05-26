4668957cfe019fd1cf8797ce787e108b45f7fc1e
Du Peng <gnepud@gmail.com>
2023-05-26 18:52:14 +0200

    (feat) confirmation required for delete child

--- app/frontend/src/javascript/components/family-account/child-item.tsx
+++ app/frontend/src/javascript/components/family-account/child-item.tsx
@@ -3,18 +3,39 @@ import { Child } from '../../models/child';
 import { useTranslation } from 'react-i18next';
 import { FabButton } from '../base/fab-button';
 import FormatLib from '../../lib/format';
+import { DeleteChildModal } from './delete-child-modal';
+import ChildAPI from '../../api/child';
 
 interface ChildItemProps {
   child: Child;
   onEdit: (child: Child) => void;
-  onDelete: (child: Child) => void;
+  onDelete: (error: string) => void;
+  onError: (error: string) => void;
 }
 
 /**
  * A child item.
  */
-export const ChildItem: React.FC<ChildItemProps> = ({ child, onEdit, onDelete }) => {
+export const ChildItem: React.FC<ChildItemProps> = ({ child, onEdit, onDelete, onError }) => {
   const { t } = useTranslation('public');
+  const [isOpenDeleteChildModal, setIsOpenDeleteChildModal] = React.useState<boolean>(false);
+
+  /**
+   * Toggle the delete child modal
+   */
+  const toggleDeleteChildModal = () => setIsOpenDeleteChildModal(!isOpenDeleteChildModal);
+
+  /**
+   * Delete a child
+   */
+  const deleteChild = () => {
+    ChildAPI.destroy(child.id).then(() => {
+      toggleDeleteChildModal();
+      onDelete(t('app.public.child_item.deleted'));
+    }).catch(() => {
+      onError(t('app.public.child_item.unable_to_delete'));
+    });
+  };
 
   return (
     <div className="child-item">
@@ -32,7 +53,8 @@ export const ChildItem: React.FC<ChildItemProps> = ({ child, onEdit, onDelete })
       </div>
       <div className="actions">
         <FabButton icon={<i className="fa fa-edit" />} onClick={() => onEdit(child)} className="edit-button" />
-        <FabButton icon={<i className="fa fa-trash" />} onClick={() => onDelete(child)} className="delete-button" />
+        <FabButton icon={<i className="fa fa-trash" />} onClick={toggleDeleteChildModal} className="delete-button" />
+        <DeleteChildModal isOpen={isOpenDeleteChildModal} toggleModal={toggleDeleteChildModal} child={child} onDelete={deleteChild} />
       </div>
     </div>
   );
