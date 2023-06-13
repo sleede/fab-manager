e28286d3cc92f4d9054f4a491f590f38ba0975c2
Du Peng <gnepud@gmail.com>
2023-06-13 11:38:06 +0200

    (wip) add pre-registration to event

--- db/structure.sql
+++ db/structure.sql
@@ -1244,7 +1244,9 @@ CREATE TABLE public.events (
     age_range_id integer,
     category_id integer,
     deleted_at timestamp without time zone,
-    event_type character varying DEFAULT 'standard'::character varying
+    event_type character varying DEFAULT 'standard'::character varying,
+    pre_registration boolean DEFAULT false,
+    pre_registration_end_date timestamp(6) without time zone
 );
 
 
@@ -8937,6 +8939,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20230524080448'),
 ('20230524083558'),
 ('20230524110215'),
-('20230525101006');
+('20230525101006'),
+('20230612123250');
 
 
