5a23b8d5e55c7a12bffa90322fc70293ae7127d7
Du Peng <gnepud@gmail.com>
2023-07-27 17:47:22 +0200

    (bug) unable to remove supporting document type

--- app/models/supporting_document_refusal.rb
+++ app/models/supporting_document_refusal.rb
@@ -2,8 +2,9 @@
 
 # An admin can mark an uploaded document as refused, this will notify the member
 class SupportingDocumentRefusal < ApplicationRecord
+  include NotificationAttachedObject
+
   belongs_to :supportable, polymorphic: true
   belongs_to :operator, class_name: 'User', inverse_of: :supporting_document_refusals
-  has_many :supporting_document_refusals_types, dependent: :destroy
-  has_many :supporting_document_types, through: :supporting_document_refusals_types
+  has_and_belongs_to_many :supporting_document_types
 end
