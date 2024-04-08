b4c535149e87063fc673bbd5cc6c29f30279f98e
Du Peng <gnepud@gmail.com>
2024-04-08 09:26:17 +0200

    (fix) remove unused table in structure.sal

--- db/structure.sql
+++ db/structure.sql
@@ -2243,41 +2243,6 @@ ALTER SEQUENCE public.payment_gateway_objects_id_seq OWNED BY public.payment_gat
 
 
 --
--- Name: payment_infos; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.payment_infos (
-    id bigint NOT NULL,
-    data jsonb,
-    state character varying,
-    payment_for character varying,
-    service character varying,
-    statistic_profile_id bigint,
-    created_at timestamp(6) without time zone NOT NULL,
-    updated_at timestamp(6) without time zone NOT NULL
-);
-
-
---
--- Name: payment_infos_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.payment_infos_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: payment_infos_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.payment_infos_id_seq OWNED BY public.payment_infos.id;
-
-
---
 -- Name: payment_schedule_items; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -4398,8 +4363,8 @@ CREATE TABLE public.users (
     is_allow_newsletter boolean,
     current_sign_in_ip inet,
     last_sign_in_ip inet,
-    validated_at timestamp without time zone,
     mapped_from_sso character varying,
+    validated_at timestamp without time zone,
     supporting_documents_reminder_sent_at timestamp(6) without time zone
 );
 
@@ -4950,13 +4915,6 @@ ALTER TABLE ONLY public.organizations ALTER COLUMN id SET DEFAULT nextval('publi
 --
 
 ALTER TABLE ONLY public.payment_gateway_objects ALTER COLUMN id SET DEFAULT nextval('public.payment_gateway_objects_id_seq'::regclass);
-
-
---
--- Name: payment_infos id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.payment_infos ALTER COLUMN id SET DEFAULT nextval('public.payment_infos_id_seq'::regclass);
 
 
 --
@@ -5893,14 +5851,6 @@ ALTER TABLE ONLY public.organizations
 
 ALTER TABLE ONLY public.payment_gateway_objects
     ADD CONSTRAINT payment_gateway_objects_pkey PRIMARY KEY (id);
-
-
---
--- Name: payment_infos payment_infos_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.payment_infos
-    ADD CONSTRAINT payment_infos_pkey PRIMARY KEY (id);
 
 
 --
@@ -7117,13 +7067,6 @@ CREATE INDEX index_payment_gateway_objects_on_payment_gateway_object_id ON publi
 
 
 --
--- Name: index_payment_infos_on_statistic_profile_id; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX index_payment_infos_on_statistic_profile_id ON public.payment_infos USING btree (statistic_profile_id);
-
-
---
 -- Name: index_payment_schedule_items_on_invoice_id; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -7915,6 +7858,14 @@ CREATE UNIQUE INDEX unique_not_null_external_id ON public.invoicing_profiles USI
 
 
 --
+-- Name: accounting_periods accounting_periods_del_protect; Type: RULE; Schema: public; Owner: -
+--
+
+CREATE RULE accounting_periods_del_protect AS
+    ON DELETE TO public.accounting_periods DO INSTEAD NOTHING;
+
+
+--
 -- Name: accounting_periods accounting_periods_upd_protect; Type: RULE; Schema: public; Owner: -
 --
 
@@ -7945,14 +7896,6 @@ CREATE TRIGGER projects_search_content_trigger BEFORE INSERT OR UPDATE ON public
 
 ALTER TABLE ONLY public.payment_schedules
     ADD CONSTRAINT fk_rails_00308dc223 FOREIGN KEY (wallet_transaction_id) REFERENCES public.wallet_transactions(id);
-
-
---
--- Name: payment_infos fk_rails_0308366a58; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.payment_infos
-    ADD CONSTRAINT fk_rails_0308366a58 FOREIGN KEY (statistic_profile_id) REFERENCES public.statistic_profiles(id);
 
 
 --
@@ -9301,10 +9244,8 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20230328094808'),
 ('20230328094809'),
 ('20230331132506'),
-('20230509121907'),
 ('20230509161557'),
 ('20230510141305'),
-('20230511080650'),
 ('20230511081018'),
 ('20230524080448'),
 ('20230524083558'),
@@ -9320,7 +9261,6 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20230720085857'),
 ('20230728072726'),
 ('20230728090257'),
-('20230825101952'),
 ('20230828073428'),
 ('20230831103208'),
 ('20230901090637'),
