1b9a1f4ab1af2e73b04953ef0d0cd6cf528b9946
Du Peng <gnepud@gmail.com>
2025-08-21 17:27:55 +0200

    (feat) dodoc module cache

--- app/models/do_doc.rb
+++ app/models/do_doc.rb
@@ -4,4 +4,14 @@
 class DoDoc < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :url, presence: true
+
+  # Clear DoDoc projects cache when DoDoc changes
+  after_save :invalidate_projects_cache
+  after_destroy :invalidate_projects_cache
+
+  private
+
+  def invalidate_projects_cache
+    DoDocProjectsService.invalidate_cache
+  end
 end
